b258437bf5fce979d1ca36b9d220590d025587d3
faridullah77 <faridullahkhan96@gmail.com>
2024-09-26 19:22:43 +0500

    starting arrays program

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+// File: arrays.cpp
+// Author: Faridullah
+// Created on: 2023-10-06
+// Last Modified: [Insert Date]
+
+#include <iostream>
+#include <vector>
+
+// Add your function declarations and implementations here
+
+int main() {
+    // Your code here
+    return 0;
+}
