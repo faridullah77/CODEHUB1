f6f54916695c0ac9f3afee96868d88976264eeb4
faridullah77 <faridullahkhan96@gmail.com>
2024-09-26 19:58:32 +0500

    array program written

--- arrays.cpp
+++ arrays.cpp
@@ -1,14 +1,42 @@
 // File: arrays.cpp
 // Author: Faridullah
 // Created on: 2023-10-06
-// Last Modified: [Insert Date]
+// Last Modified: 2023-10-06
 
-#include <iostream>
-#include <vector>
+    #include <iostream>
+    #include <vector>
 
-// Add your function declarations and implementations here
+    using namespace std;
 
-int main() {
-    // Your code here
-    return 0;
-}
+    // Add your function declarations and implementations here
+
+    int main() {
+        int ages[3];
+        int ages2[3];
+        bool is_equal = true;
+        // asking users for input
+        for (int i = 0; i < 3; i++) {
+            cin >> ages[i];
+        }
+        for (int i = 0; i < 3; i++) {
+            cin >> ages2[i];
+        }
+        // comparing the two arrays
+        for (int i = 0; i < 3; i++) {
+            if (ages[i] != ages2[i]) {
+                is_equal = false;
+                break;
+            }
+        }
+        if (is_equal) {
+            cout << "The arrays are equal" << endl;
+        } else {
+            cout << "The arrays are not equal" << endl;
+        }
+
+        return 0;
+    }
+
+
+
+
